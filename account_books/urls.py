04de7cdc18e165ffa09eba279f1cff453dedeee1
DGK <rlaehdrb0923@gmail.com>
2022-07-15 07:01:12 +0900

    feat: 가계부 카테고리 생성, 조회리스트, 수정, 삭제, 복구 기능구현 #12

--- account_books/urls.py
+++ account_books/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from account_books.views import AccountBookView, AccountBookDetailView, AccountBookRestoreView
+from account_books.views import AccountBookView, AccountBookDetailView, AccountBookRestoreView,\
+                                AccountBookCategoryView, AccountBookCategoryDetailView, AccountBookeCategoryRestoreView
 
 urlpatterns = [
     path('', AccountBookView.as_view()),
     path('/<int:account_book_id>', AccountBookDetailView.as_view()),
     path('/<int:account_book_id>/restore', AccountBookRestoreView.as_view()),
+    path('/categories', AccountBookCategoryView.as_view()),
+    path('/categories/<int:account_book_category_id>', AccountBookCategoryDetailView.as_view()),
+    path('/categories/<int:account_book_category_id>/restore', AccountBookeCategoryRestoreView.as_view()),
 ]
--- account_books/views.py
+++ account_books/views.py
@@ -8,20 +8,22 @@ from rest_framework.response    import Response
 from rest_framework.permissions import IsAuthenticated
 
 from account_books.models       import AccountBook
-from account_books.serializers  import AccountBookSerializer, AccountBookDetailSerializer
+from account_books.serializers  import AccountBookSerializer, AccountBookDetailSerializer, AccountBookCategorySerializer,\
+                                       AccountBookCategoryDetailSerializer
 
-from account_books.models       import AccountBook
-from core.utils                 import GetAccountBook
+from account_books.models       import AccountBook, AccountBookCategory
+from core.utils                 import GetAccountBook, GetAccountBookCategory
+
 
 class AccountBookView(APIView):
     
     permission_classes = [IsAuthenticated]
     
-    sort   = openapi.Parameter('sort', openapi.IN_QUERY, required=False, pattern="?sort=", type=openapi.TYPE_STRING)
-    search = openapi.Parameter('search', openapi.IN_QUERY, required=False, pattern="?search=", type=openapi.TYPE_STRING)
-    status = openapi.Parameter('status', openapi.IN_QUERY, required=False, pattern="?status=", type=openapi.TYPE_STRING)
-    offset = openapi.Parameter('offset', openapi.IN_QUERY, required=False, pattern="?offset=", type=openapi.TYPE_STRING)
-    limit  = openapi.Parameter('limit', openapi.IN_QUERY, required=False, pattern="?limit=", type=openapi.TYPE_STRING)
+    sort   = openapi.Parameter('sort', openapi.IN_QUERY, required=False, pattern='?sort=', type=openapi.TYPE_STRING)
+    search = openapi.Parameter('search', openapi.IN_QUERY, required=False, pattern='?search=', type=openapi.TYPE_STRING)
+    status = openapi.Parameter('status', openapi.IN_QUERY, required=False, pattern='?status=', type=openapi.TYPE_STRING)
+    offset = openapi.Parameter('offset', openapi.IN_QUERY, required=False, pattern='?offset=', type=openapi.TYPE_STRING)
+    limit  = openapi.Parameter('limit', openapi.IN_QUERY, required=False, pattern='?limit=', type=openapi.TYPE_STRING)
     
     @swagger_auto_schema(responses={200: AccountBookSerializer}, manual_parameters=[sort, search, status, offset, limit])
     def get(self, request):
@@ -80,7 +82,7 @@ class AccountBookDetailView(APIView):
         
         book, err = GetAccountBook.get_book_n_check_error(account_book_id, user)
         if err:
-            return Response({"detail" : err}, status=400)
+            return Response({'detail': err}, status=400)
         
         serializer = AccountBookDetailSerializer(book, data=request.data, partial=True)
         if serializer.is_valid():
@@ -89,14 +91,14 @@ class AccountBookDetailView(APIView):
         return Response(serializer.errors, status=400)
     
     account_book_id = openapi.Parameter('account_book_id', openapi.IN_PATH, required=True, type=openapi.TYPE_INTEGER)
-    
-    @swagger_auto_schema(responses={200: '가계부가 성공적으로 삭제되었습니다.'}, manual_parameters=[account_book_id])
+
+    @swagger_auto_schema(responses={200: '가계부가 삭제되었습니다.'}, manual_parameters=[account_book_id])
     def delete(self, request, account_book_id):
         user = request.user
         
         book, err = GetAccountBook.get_book_n_check_error(account_book_id, user)
         if err:
-            return Response({"detail" : err}, status=400)
+            return Response({'detail': err}, status=400)
         
         if book.status == 'deleted':
             return Response({'detail': f'가계부 {account_book_id}(id)는 이미 삭제된 상태입니다.'}, status=400)
@@ -112,13 +114,13 @@ class AccountBookRestoreView(APIView):
     
     account_book_id = openapi.Parameter('account_book_id', openapi.IN_PATH, required=True, type=openapi.TYPE_INTEGER)
     
-    @swagger_auto_schema(responses={200: '가계부가 성공적으로 복구되었습니다.'}, manual_parameters=[account_book_id])
+    @swagger_auto_schema(responses={200: '가계부가 복구되었습니다.'}, manual_parameters=[account_book_id])
     def patch(self, request, account_book_id):
         user = request.user
         
         book, err = GetAccountBook.get_book_n_check_error(account_book_id, user)
         if err:
-            return Response({"detail" : err}, status=400)
+            return Response({'detail': err}, status=400)
         
         if book.status == 'in_use':
             return Response({'detail': f'가계부 {account_book_id}(id)는 이미 사용중입니다.'}, status=400)
@@ -126,3 +128,120 @@ class AccountBookRestoreView(APIView):
         book.status = 'in_use'
         book.save()
         return Response({'detail': f'가계부 {account_book_id}(id)가 복구되었습니다.'}, status=200)
+    
+
+class AccountBookCategoryView(APIView):
+    
+    permission_classes = [IsAuthenticated]
+    
+    sort   = openapi.Parameter('sort', openapi.IN_QUERY, required=False, pattern='?sort=', type=openapi.TYPE_STRING)
+    search = openapi.Parameter('search', openapi.IN_QUERY, required=False, pattern='?search=', type=openapi.TYPE_STRING)
+    status = openapi.Parameter('status', openapi.IN_QUERY, required=False, pattern='?status=', type=openapi.TYPE_STRING)
+    offset = openapi.Parameter('offset', openapi.IN_QUERY, required=False, pattern='?offset=', type=openapi.TYPE_STRING)
+    limit  = openapi.Parameter('limit', openapi.IN_QUERY, required=False, pattern='?limit=', type=openapi.TYPE_STRING)
+    
+    @swagger_auto_schema(responses={200: AccountBookCategorySerializer}, manual_parameters=[sort, search, status, offset, limit])
+    def get(self, request):
+        user   = request.user
+            
+        search = request.GET.get('search', None)
+        sort   = request.GET.get('sort', 'up_to_date')
+        status = request.GET.get('status', 'deleted')
+        offset = int(request.GET.get('offset', 0))
+        limit  = int(request.GET.get('limit', 10))
+            
+        sort_set = {
+            'up_to_date' : '-created_at',
+            'out_of_date': 'created_at'
+        }
+        
+        q = Q()
+        
+        if user:
+            q &= Q(users=user)
+        
+        if search:
+            q |= Q(name__icontains = search)
+            
+        categories = AccountBookCategory.objects\
+                                        .select_related('users')\
+                                        .filter(q)\
+                                        .exclude(status__iexact=status)\
+                                        .order_by(sort_set[sort])[offset:offset+limit]
+                                        
+        serializer = AccountBookCategorySerializer(categories, many=True)
+        return Response(serializer.data, status=200)
+    
+    @swagger_auto_schema(request_body=AccountBookCategorySerializer, responses={201: AccountBookCategorySerializer})
+    def post(self, request):
+        user = request.user
+        
+        serializer = AccountBookCategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(users=user)
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+        
+
+
+class AccountBookCategoryDetailView(APIView):
+    
+    permission_classes = [IsAuthenticated]
+
+    account_book_category_id = openapi.Parameter('account_book_category_id', openapi.IN_PATH, required=True, type=openapi.TYPE_INTEGER)
+
+    @swagger_auto_schema(
+        request_body=AccountBookCategoryDetailSerializer, responses={200: AccountBookCategoryDetailSerializer},\
+        manual_parameters=[account_book_category_id]
+    )
+    def patch(self, request, account_book_category_id):
+        user = request.user
+        
+        category, err = GetAccountBookCategory.get_category_n_check_error(account_book_category_id, user)
+        if err:
+            return Response({'detail': err}, status=400)
+        
+        serializer = AccountBookCategoryDetailSerializer(category, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=200)
+        return Response(serializer.errors, status=400)
+
+    account_book_category_id = openapi.Parameter('account_book_category_id', openapi.IN_PATH, required=True, type=openapi.TYPE_INTEGER)
+
+    @swagger_auto_schema(responses={200: '가계부 카테고리가 삭제되었습니다.'}, manual_parameters=[account_book_category_id])
+    def delete(self, request, account_book_category_id):
+        user = request.user
+        
+        category, err = GetAccountBookCategory.get_category_n_check_error(account_book_category_id, user)
+        if err:
+            return Response({'detail': err}, status=400)
+        
+        if category.status == 'deleted':
+            return Response({'detail': f'가계부 카테고리 {account_book_category_id}(id)는 이미 삭제된 상태입니다.'}, status=400)
+        
+        category.status = 'deleted'
+        category.save()
+        return Response({'detail': f'가계부 카테고리 {account_book_category_id}(id)가 삭제되었습니다.'}, status=200)
+
+
+class AccountBookeCategoryRestoreView(APIView):
+    
+    permission_classes = [IsAuthenticated]
+    
+    account_book_category_id = openapi.Parameter('account_book_category_id', openapi.IN_PATH, required=True, type=openapi.TYPE_INTEGER)
+
+    @swagger_auto_schema(responses={200: '가계부 카테고리가 복구되었습니다.'}, manual_parameters=[account_book_category_id])
+    def patch(self, request, account_book_category_id):
+        user = request.user
+        
+        category, err = GetAccountBookCategory.get_category_n_check_error(account_book_category_id, user)
+        if err:
+            return Response({'detail': err}, status=400)
+        
+        if category.status == 'in_use':
+            return Response({'detail': f'가계부 카테고리 {account_book_category_id}(id)는 이미 사용중입니다.'}, status=400)
+
+        category.status = 'in_use'
+        category.save()
+        return Response({'detail': f'가계부 카테고리 {account_book_category_id}(id)가 복구되었습니다.'}, status=200)
